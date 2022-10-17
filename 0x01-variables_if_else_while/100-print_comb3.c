19bbdbb2ae9b2bd310029d39b9d6e11234fa2729
eioforkansi <eioforkansi@gmail.com>
2022-10-17 15:11:20 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 /**
  * main - start
- * Return:
+ * Return: end
  */
 int main(void)
 {
 	int x;
 	int y;
 
-	for (x = 0; x <= 9; x++)
+	for (x = 0; x < 9; x++)
 	{
-		for (y = x + 1; y <= 10; y++)
+		for (y = x + 1; y < 10; y++)
 		{
 			putchar(x + '0');
 			putchar(y + '0');
