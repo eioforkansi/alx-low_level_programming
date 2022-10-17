7895bbb356742cc901494cc2531cc66a51135a43
eioforkansi <eioforkansi@gmail.com>
2022-10-17 15:07:12 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - start
+ * Return:
+ */
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = x + 1; y <= 10; y++)
+		{
+			putchar(x + '0');
+			putchar(y + '0');
+
+			if (x == 8 && y == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
