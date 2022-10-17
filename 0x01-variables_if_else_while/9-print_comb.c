e98b6f525f17a702615186f40722add44c313e61
eioforkansi <eioforkansi@gmail.com>
2022-10-17 13:46:20 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - start
+ * Return: ebd
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+	if (i == 9)
+		continue;
+
+	putchar(',');
+	putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
