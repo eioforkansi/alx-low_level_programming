cd5994a227652315bb75dc8179fdd5fc47d25efc
eioforkansi <eioforkansi@gmail.com>
2022-10-20 02:29:07 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,8 +10,8 @@ void print_alphabet_x10(void)
 	char j;
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 'a'; j <= 'z'; j++)
-			_putchar(j);
-			_putchar('\n');
+	for (j = 'a'; j <= 'z'; j++)
+	_putchar(j);
+	_putchar('\n');
 	}
 }
