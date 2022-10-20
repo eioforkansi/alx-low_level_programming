6530549e749937a472994a4b4721293fc88a0423
eioforkansi <eioforkansi@gmail.com>
2022-10-20 09:31:13 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *print_sign - start
+ * @n: input parameter
+ *Return: end
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	_putchar('+');
+	return (1);
+	}
+	if (n < 0)
+	{
+	_putchar('-');
+	return (-1);
+	}
+	if (n == 0)
+	{
+	_putchar('0');
+	return (0);
+	}
+	return (0);
+}
