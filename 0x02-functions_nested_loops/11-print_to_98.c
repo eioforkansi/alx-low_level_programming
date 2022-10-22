afda789a578d41b2f3465b08f0cbb66e4fb818fd
eioforkansi <eioforkansi@gmail.com>
2022-10-22 05:44:55 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,12 +9,16 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		for (n ; n <= 98; n++)
+		for (n = n; n <= 98; n++)
+		{
 			printf("%d, ", n);
+		}
 	}
 	else
 	{
-		for (n ; n >= 98; n--)
+		for (n = n; n > 98; n--)
+		{
 			printf("%d, ", n);
+		}
 	}
 }
