12662603ba6400b653845868fdf9555afb6bc1ba
eioforkansi <eioforkansi@gmail.com>
2022-10-22 04:53:00 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *print_to_98 - start
+ *@n: input argument
+ *Return: end
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (n ; n <= 98; n++)
+			printf("%d, ", n);
+	}
+	else
+	{
+		for (n ; n >= 98; n--)
+			printf("%d, ", n);
+	}
+}
