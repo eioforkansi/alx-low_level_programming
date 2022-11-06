2fa155011be36129bddec4770f91b06f2450773e
eioforkansi <eioforkansi@gmail.com>
2022-11-06 11:04:19 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: Number of arguments passed to main function
+ * @argv: Pointer to a string array containing the names of arguments passed
+ * Return: 0 value for success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
