aa11714a93c1e6049dcfdea59d5028741f9e1574
eioforkansi <eioforkansi@gmail.com>
2022-11-13 00:45:46 -0800

    2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * _strlen - returns length of string
+ * @s: string
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
+ *str_concat - function that creates copy of string argument passed
+ *@s1: string to be updated
+ *@s2: string to be copied
+ *Return: NULL if insuficient memory,or else a pointer
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i, l1, l2, l3;
+	char *p;
+
+	if (s1 == NULL || s2 == NULL)
+	{
+		return (NULL);
+	}
+	l1 = _strlen(s1);
+	l2 = _strlen(s2);
+	l3 = l1 + l2;
+	p = malloc(sizeof(char) * l3);
+	if (p == 0)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < l1; i++)
+	{
+		p[i] = s1[i];
+	}
+	for (i = 0; i <= l2; i++)
+	{
+		p[l1 + i] = s2[i];
+	}
+	return (p);
+}
