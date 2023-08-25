d4514e1a74b50b7b7ef64fee4ecc2c3d9dd902aa
root <you@example.com>
2023-08-25 05:39:09 -0700

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - function main
+ *@argc: Number of arguments
+ *@argv: Array of arguments
+ *Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+	{
+	printf("0\n");
+
+	}
+	int i;
+	int j;
+	int sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+
+		}
+		int result = atoi(argv[i]);
+
+		sum += result;
+	}
+	printf("%d\n", sum);
+}
+
