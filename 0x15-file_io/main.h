32981b2838f12afc2b77fc1947f19fccb5cfd45b
eioforkansi <eioforkansi@gmail.com>
2023-09-04 20:58:33 +0000

    0x15-file_io Project

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,5 @@
 #ifndef MAIN_H
-#define MAIN_N
+#define MAIN_H
 
 #include <elf.h>
 #include <stdlib.h>
@@ -14,8 +14,8 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
 /**
-   * ELF header functions
-   */
+  * ELF header functions
+  */
 void check_elf(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
 void print_class(unsigned char *e_ident);
