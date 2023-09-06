a54a71dca33280134ea685cb34a2f3c16a0ace6d
eioforkansi <eioforkansi@gmail.com>
2023-09-06 13:55:19 +0000

    A tough one

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,40 +1,36 @@
 #include "main.h"
-#include <stdlib.h>
+#include <stddef.h>
 
 /**
- * read_textfile - Function that reads a text file
- *				   and prints it to POSIX stdout.
- * @filename: A pointer to the name of the file.
- * @letters: The number of letters the
- *           function should read and print.
- *
- * Return: If the function fails or filename is NULL - 0.
- *         O/w - the actual number of bytes the function can read and print.
- */
+* read_textfile - Function that reads a text file
+*                 and prints it to the std output
+* @filename: name of the file
+* @letters:  no of letters to be printed
+*
+* Return: no of letters read and printed
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r, w;
+	int fls, fread, wrt;
 	char *buffer;
 
-	if (filename == NULL)
-		return (0);
-
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-		return (0);
-
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
-
-	if (o == -1 || r == -1 || w == -1 || w != r)
+	buffer = malloc(sizeof(*buffer) * (letters + 1));
+	if (filename == NULL || buffer == NULL)
 	{
 		free(buffer);
 		return (0);
 	}
-
+	fls = open(filename, O_RDONLY);
+	if (fls == -1)
+		return (0);
+	fread = read(fls, buffer, letters);
+	if (fread == -1)
+		return (0);
+	buffer[fread] = '\0';
+	wrt = write(STDOUT_FILENO, buffer, fread);
+	if (wrt != fread)
+		return (0);
 	free(buffer);
-	close(o);
-
-	return (w);
+	close(fls);
+	return (fread);
 }
