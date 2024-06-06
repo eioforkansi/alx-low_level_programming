e5eef3d3b1115a26859cb07f039d11978929c829
eioforkansi <eioforkansi@gmail.com>
2024-06-06 20:17:37 +0000

    Makefiles in C

---
+++ 0x1C-makefiles/README.md
@@ -0,0 +1,1 @@
+In this project I'm learning Makefiles and it use cases.
---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,15 @@
+#include "m.h"
+
+/**
+ * main - Entry point
+ *
+ *Rturn: Always
+ */
+
+int main(void)
+{
+	print_school();
+	return (EXIT_SUCCESS);
+}
+
+/* School */
